d6d40f4370980655d8a83d6c04d5a7ca29ffd185
PrinceB3N <benjaminprince@ucsb.edu>
2020-01-29 14:00:03 -0800

    test

--- android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/MapsActivity.java
+++ android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/MapsActivity.java
@@ -3,6 +3,7 @@ package edu.ucsb.cs.cs48.schedoptim;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,7 +13,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+//public class MapsActivity extends FragmentActivity{
     private GoogleMap mMap;
 
     @Override
@@ -23,6 +24,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+    public void setText(String text){
+        TextView textView = (TextView) findViewById(R.id.text_hello_world);
+        textView.setText(text);
     }
 
 
@@ -35,6 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -44,4 +51,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+
+
 }

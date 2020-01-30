17acf7e3aa677baf1f2901ae1a672e748eb1dcef
PrinceB3N <benjaminprince@umail.ucsb.edu>
2020-01-29 16:33:28 -0800

    Update MapsActivity.java

--- android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/MapsActivity.java
+++ android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/MapsActivity.java
@@ -19,11 +19,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        //setContentView(R.layout.activity_maps);
+        setContentView(R.layout.hello_world);
+        setText("Hello World");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        /*SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        */
 
     }
     public void setText(String text){

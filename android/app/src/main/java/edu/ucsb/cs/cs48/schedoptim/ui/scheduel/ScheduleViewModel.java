5a45f945677e186753a8a65714b04c6df1fc1f46
Maynic <zye03@ucsb.edu>
2020-02-04 16:39:28 -0800

    my - reorganize files

--- android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/ui/scheduel/ScheduleViewModel.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs.cs48.schedoptim.ui.scheduel;
-
-import androidx.lifecycle.ViewModel;
-
-public class ScheduleViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/ui/schedule/ScheduleViewModel.java
+++ android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/ui/schedule/ScheduleViewModel.java
@@ -1,16 +1,16 @@
-package edu.ucsb.cs.cs48.schedoptim.ui.home;
+package edu.ucsb.cs.cs48.schedoptim.ui.schedule;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ScheduleViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ScheduleViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is schedule fragment");
     }
 
     public LiveData<String> getText() {

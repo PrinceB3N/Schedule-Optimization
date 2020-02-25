ea23b10d156b77805407bcfe45e66a6e4f02f9f7
PrinceB3N <benjaminprince@ucsb.edu>
2020-02-24 16:20:42 -0800

    Merge branch 'Benjamin' of https://github.com/ucsb-cs48-w20/5pm-schedule-optimization
    # Conflicts:
    #	android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/MapsActivity.java

---
+++ android/app/src/main/java/edu/ucsb/cs/cs48/schedoptim/RouteDatabase.java
@@ -0,0 +1,57 @@
+package edu.ucsb.cs.cs48.schedoptim;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+
+@Database(entities = { Route.class}, version = 2,exportSchema = false)
+public abstract class RouteDatabase extends RoomDatabase {
+
+    //    private static final String DB_NAME = "RouteDatabase.db";
+//    private static volatile RouteDatabase instance;
+    public abstract RouteDao getrouteDao();
+    private static RouteDatabase INSTANCE;//创建单例
+
+    static RouteDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (RouteDatabase.class) {
+                if (INSTANCE == null)    {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            RouteDatabase.class, "route_database")
+//                            .addCallback(sOnOpenCallback)
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;}
+//
+//    private static RoomDatabase.Callback sOnOpenCallback =
+//            new RoomDatabase.Callback(){
+//                @Override
+//                public void onOpen (@NonNull SupportSQLiteDatabase db){
+//                    super.onOpen(db);
+//                    initializeData();
+//                }};
+
+//    static synchronized RouteDatabase getInstance(Context context) {
+//        if (instance == null) {
+//            instance = create(context);
+//        }
+//        return instance;
+//    }
+//
+//    private static RouteDatabase create(final Context context) {
+//        return Room.databaseBuilder(
+//                context,
+//                RouteDatabase.class,
+//                DB_NAME).build();
+//    }
+
+}
